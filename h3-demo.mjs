1fda9a083895bf524085c6f27c84dc9d5bdf5aed
gumingWu <15626185054@163.com>
2023-06-05 23:41:09 +0800

    http demo

--- h3-demo.mjs
+++ h3-demo.mjs
@@ -10,31 +10,31 @@ app.use('/', eventHandler(() => '<h1>hi, im h3</h1>'))
 //   console.log('Server is running on: http://localhost:8888')
 // })
 
-// const listener = await listen(toNodeListener(app), {
-//   port: 8888,
-//   showURL: false,
-//   open: false,
-//   clipboard: true,
-// })
-
-const loadingListener = (req, res) => {
-  res.setHeader('Content-Type', 'text/html; charset=UTF-8')
-  res.statusCode = 503
-  res.end(loading({ loading: 'Starting' }))
-}
-let currentListener
-const serverHandler = (req, res) => {
-  return currentListener ? currentListener(req, res) : loadingListener(req, res)
-}
-
-await listen(serverHandler, {
+await listen(toNodeListener(app), {
   port: 8888,
   showURL: true,
   open: false,
   clipboard: true,
 })
 
-// ! 需要找出如何修改listener的拦截器
-setTimeout(() => {
-  currentListener = toNodeListener(app)
-}, 5000)
+// const loadingListener = (req, res) => {
+//   res.setHeader('Content-Type', 'text/html; charset=UTF-8')
+//   res.statusCode = 503
+//   res.end(loading({ loading: 'Starting' }))
+// }
+// let currentListener
+// const serverHandler = (req, res) => {
+//   return currentListener ? currentListener(req, res) : loadingListener(req, res)
+// }
+
+// await listen(serverHandler, {
+//   port: 8888,
+//   showURL: true,
+//   open: false,
+//   clipboard: true,
+// })
+
+// // ! 需要找出如何修改listener的拦截器
+// setTimeout(() => {
+//   currentListener = toNodeListener(app)
+// }, 5000)
--- http-demo.mjs
+++ http-demo.mjs
@@ -1,9 +1,119 @@
 import { createServer } from 'node:http'
 
+// 初始服务器
+// const server = createServer((req, res) => {
+//   if (req.url === '/') {
+//     res.setHeader('Content-Type', 'text/html')
+//     res.end('<h1>hi, im http</h1>')
+//   }
+// })
+
+// server.listen(9999, () => {
+//   console.log('Server is running on: http://localhost:9999')
+// })
+
+// 新增get请求
+// const server = createServer((req, res) => {
+//   if (req.url === '/') {
+//     res.setHeader('Content-Type', 'text/html')
+//     res.end(`\
+//     <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//   <meta charset="UTF-8">
+//   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//   <title>Document</title>
+// </head>
+// <body>
+//   <h1>hi, im http</h1>
+//   <button id="button">get请求</button>
+
+//   <script>
+//     const button = document.getElementById('button')
+//     button.addEventListener('click', () => {
+//       const request = new XMLHttpRequest()
+//       request.open('GET', 'http://localhost:9999/user')
+//       request.send()
+//     })
+//   </script>
+// </body>
+// </html>
+//     `)
+//   }
+//   if (req.url === '/user') {
+//     res.setHeader('Content-Type', 'application/json')
+//     res.end(JSON.stringify({
+//       name: 'wu',
+//       age: 18,
+//     }))
+//   }
+// })
+// server.listen(9999, () => {
+//   console.log('Server is running on: http://localhost:9999')
+// })
+
+// 新增post请求
 const server = createServer((req, res) => {
   if (req.url === '/') {
     res.setHeader('Content-Type', 'text/html')
-    res.end('<h1>hi, im http</h1>')
+    res.end(`\
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>hi, im http</h1>
+  <button id="get">get请求</button>
+  <button id="post">post请求</button>
+
+  <script>
+    const get = document.getElementById('get')
+    get.addEventListener('click', () => {
+      const request = new XMLHttpRequest()
+      request.open('GET', 'http://localhost:9999/user')
+      request.send()
+    })
+
+    const post = document.getElementById('post')
+    post.addEventListener('click', () => {
+      const request = new XMLHttpRequest()
+      request.open('POST', 'http://localhost:9999/post')
+      request.send(JSON.stringify({ // 这里带请求参数
+        name: 'wu',
+      }))
+    })
+  </script>
+</body>
+</html>
+    `)
+  }
+  if (req.url === '/user') {
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify({
+      name: 'wu',
+      age: 18,
+    }))
+  }
+})
+
+server.on('request', (req, res) => {
+  if (req.url === '/post' && req.method === 'POST') {
+    const body = []
+    req.on('data', (chunk) => {
+      body.push(chunk)
+    }).on('end', () => {
+      console.log(Buffer.concat(body).toString())
+      res.setHeader('Content-Type', 'application/json')
+      res.end(JSON.stringify({
+        name: 'wu',
+        age: 18,
+      }))
+    })
   }
 })
 
